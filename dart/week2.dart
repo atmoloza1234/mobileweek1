c28d326486d04cc7c84b2b026812faa425e2e8f6
Nawa <atmoloza1234@gmail.com>
2021-08-05 17:06:09 +0700

    comment แต่ละการใช้งาน

--- dart/week2.dart
+++ dart/week2.dart
@@ -1,16 +1,15 @@
 void main() {
   //print(callGen(2000));
   //print(callColor());
+  // เรียกใช้ funtion callWeb
   callWeb("Google",protocal: "https",port : 443);
 }
 
-
+//การเขียนเงื่อนไขแบบ shot-hand
 // การ return ภายในบรรทัดเดียว
 //String callGen(int year) => (year >= 1996) ? "Gen Z" : "Gen Y";
-
 //void callGen(int year){
 //var year = 2000;
-
 /*if(year >= 1996){
     print("Gen Z");
   }  else {
@@ -19,9 +18,10 @@ void main() {
 }*/
 // การเช็คเงื่อนไข         ture              false
 // return (year >= 1996) ? "Gen Z" : "Gen Y";
-
 //return x;
 
+
+//การเขียน for in และ return ข้อมูลใน list
 /*int callColor([String newColor = "white"]) {
   //          index         0    1
   List<dynamic> color = ["red", "blue", "green"];
@@ -34,4 +34,5 @@ void main() {
   return color.length;
 }*/
 
+//การเขียนการเชื่อมต่อเว็บและ port แบบการใช้ Arrow Function 
 void callWeb(String web ,{int port = 80, String protocal = "http"}) => print("$protocal $web : $port");
