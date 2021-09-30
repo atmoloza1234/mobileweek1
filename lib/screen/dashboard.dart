68dcaeb01278bb883cf005abdd0291f2c76bba2c
Nawa <atmoloza1234@gmail.com>
2021-09-30 19:24:43 +0700

    update API

--- lib/screen/dashboard.dart
+++ lib/screen/dashboard.dart
@@ -1,6 +1,11 @@
 import 'dart:ffi';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile1/model/tct.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -9,14 +14,67 @@ class Dashboard extends StatefulWidget {
   _DashboardState createState() => _DashboardState();
 }
 
+var data;
+
 class _DashboardState extends State<Dashboard> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("HELLO WORLD");
+    callAPI();
+  }
+
+  Future<void> callAPI() async {
+    var url = Uri.parse("https://www.boredapi.com/api/activity");
+    var response = await http.get(url);
+
+    setState(() {
+      data = tctFromJson(response.body);
+    });
+    //print(response.statusCode);
+    //print(response.body);
+    //print(response.request);
+
+    //data = tctFromJson(response.body);
+    //print(data.activity);
+    //print(data.type);
+    //print(data.link);
+  }
+
+  Future<void> loading() async {}
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(title: Text("Dashboard")),
+        appBar: AppBar(
+          title: Row(
+            children: [
+              Icon(Icons.api),
+              SizedBox(width: 10),
+              Text("Dashboard"),
+            ],
+          ),
+        ),
         body: SingleChildScrollView(
-          child: Container(),
+          child: Container(
+            child: Column(
+              children: [
+                Text(
+                  data?.activity ?? "loading...",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.pinkAccent.shade100,
+                    fontSize: 20,
+                  ),
+                ),
+                Text(data?.type ?? "loading..."),
+                Text('${data?.price ?? "loading..."}'),
+                Text('${data?.participants ?? "loading..."}'),
+              ],
+            ),
+          ),
         ),
         drawer: Drawer(
           // Add a ListView to the drawer. This ensures the user can scroll
@@ -36,12 +94,13 @@ class _DashboardState extends State<Dashboard> {
                 },
               ),
               ListTile(
-                title: const Text('Item 2'),
+                title: const Text('Logout'),
                 onTap: () {
                   // Update the state of the app
                   // ...
                   // Then close the drawer
-                  Navigator.pop(context);
+
+                  Navigator.popAndPushNamed(context, "Login");
                 },
               ),
             ],
